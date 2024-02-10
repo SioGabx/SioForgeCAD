95dbac90048cc1aa4097a45494cada13eb1a2008
SioGabx <41244237+SioGabx@users.noreply.github.com>
2024-02-10 12:30:54 +0100

    Change color of Grip

--- SioForgeCAD/Commun/Overrules/CopyGripOverrule/CopyGrip.cs
+++ SioForgeCAD/Commun/Overrules/CopyGripOverrule/CopyGrip.cs
@@ -24,9 +24,9 @@ namespace SioForgeCAD.Commun.Overrules
         public override bool ViewportDraw(ViewportDraw worldDraw, ObjectId entityId, DrawType type, Point3d? imageGripPoint, int gripSizeInPixels)
         {
             var unit = worldDraw.Viewport.GetNumPixelsInUnitSquare(GripPoint);
-            var gripHeight = 3.0 * gripSizeInPixels / unit.X;
+            var gripHeight = 2.5 * gripSizeInPixels / unit.X;
             Generic.WriteMessage(gripSizeInPixels + " / " + gripHeight);
-            
+
             var x = GripPoint.X;
             var y = GripPoint.Y;
             var offset = gripHeight / 2.0;
@@ -37,12 +37,68 @@ namespace SioForgeCAD.Commun.Overrules
             //points.Add(new Point3d(x + offset, y, 0.0));
             //points.Add(new Point3d(x, y + offset, 0.0));
 
-            Point3d center = new Point3d(x, y, 0.0);
-            var radius = offset;
+            //Point3d center = new Point3d(x, y, 0.0);
+            //var radius = offset;
 
-           worldDraw.SubEntityTraits.FillType = FillType.FillAlways;
-            worldDraw.SubEntityTraits.Color = 150;
-            worldDraw.Geometry.Circle(center, radius, Vector3d.ZAxis);
+            double WidthCross = ((gripHeight / 3) / 2) * 0.8;
+
+            Point3d Origin = new Point3d(x, y, 0.0);
+            Point3d OriginTop = new Point3d(x, y + offset, 0.0);
+            Point3d OriginBottom = new Point3d(x, y - offset, 0.0);
+            Point3d OriginLeft = new Point3d(x - offset, y, 0.0);
+            Point3d OriginRight = new Point3d(x + offset, y, 0.0);
+
+            Vector3d YVector = Vector3d.YAxis;
+            Vector3d XVector = Vector3d.XAxis;
+
+            Point3d Transform(Point3d point, Vector3d Vector)
+            {
+                return point.TransformBy(Matrix3d.Displacement(Vector.GetNormal().MultiplyBy(WidthCross)));
+            }
+
+            var points = new Point3dCollection
+            {
+                Transform(OriginTop, -XVector), //A
+                Transform(OriginTop, XVector), //B
+                Transform(Transform(Origin, XVector), YVector), //C
+
+                
+                Transform(OriginRight, YVector), //D
+                Transform(OriginRight, -YVector), //E
+                Transform(Transform(Origin, XVector), -YVector), //F
+
+                Transform(OriginBottom, XVector), //G
+                Transform(OriginBottom, -XVector), //H
+                Transform(Transform(Origin, -XVector), -YVector), //I
+
+                Transform(OriginLeft, -YVector), //J
+                Transform(OriginLeft, YVector), //K
+                Transform(Transform(Origin, -XVector), YVector), //L
+            };
+
+
+            worldDraw.SubEntityTraits.FillType = FillType.FillAlways;
+            if (type == DrawType.HoverGrip)
+            {
+                worldDraw.SubEntityTraits.Color = 11;
+            }
+            else if (type == DrawType.HotGrip)
+            {
+                worldDraw.SubEntityTraits.Color = 12;
+            }
+            else
+            {
+                worldDraw.SubEntityTraits.Color = 150;
+            }
+            worldDraw.Geometry.Polygon(points);
+
+            if (type == DrawType.WarmGrip)
+            {
+                worldDraw.SubEntityTraits.FillType = FillType.FillNever;
+                worldDraw.SubEntityTraits.Color = 0;
+                worldDraw.Geometry.Polygon(points);
+            }
+
             return true;
         }
 
