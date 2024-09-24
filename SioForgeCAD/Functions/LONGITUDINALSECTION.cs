de06bc901c81d849149b237c104ce2a0d4bf8490
SioGabx <contact@siogabx.fr>
2024-09-24 17:05:00 +0200

    started LONGITUDINALSECTION

---
+++ SioForgeCAD/Functions/LONGITUDINALSECTION.cs
@@ -0,0 +1,59 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using SioForgeCAD.Commun;
+using SioForgeCAD.Commun.Drawing;
+using SioForgeCAD.Commun.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SioForgeCAD.Functions.DRAWCPTERRAIN;
+
+namespace SioForgeCAD.Functions
+{
+    public static class LONGITUDINALSECTION
+    {
+        public static void DrawSectionFromSelectedPoints()
+        {
+            Database db = Generic.GetDatabase();
+            Editor ed = Generic.GetEditor();
+
+            using (Polyline TerrainBasePolyline = ed.GetPolyline("\nSélectionnez une polyligne definissant la section", false))
+            {
+                if (TerrainBasePolyline == null)
+                {
+                    return;
+                }
+                TypedValue[] EntitiesGroupCodesList = new TypedValue[1] { new TypedValue((int)DxfCode.Start, "INSERT") };
+                SelectionFilter SelectionEntitiesFilter = new SelectionFilter(EntitiesGroupCodesList);
+                PromptSelectionOptions PromptBlocSelectionOptions = new PromptSelectionOptions
+                {
+                    MessageForAdding = "\nSelectionnez des côtes à projeter",
+                    RejectObjectsOnLockedLayers = false
+                };
+                var BlockRefSelection = ed.GetSelection(PromptBlocSelectionOptions, SelectionEntitiesFilter);
+                if (BlockRefSelection.Status != PromptStatus.OK) { return; }
+
+                using (Transaction trans = db.TransactionManager.StartTransaction())
+                {
+                    ObjectId[] SelectedCoteBloc = BlockRefSelection.Value.GetObjectIds();
+
+                    Polyline profilePolyline = new Polyline();
+
+                    //TODO
+
+
+
+
+
+                    HightLighter.UnhighlightAll(SelectedCoteBloc);
+                    trans.Commit();
+                }
+            }
+        }
+
+
+    }
+}
