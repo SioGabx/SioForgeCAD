87a00dcfea16e2939f3c9027f218733aabb1df20
SioGabx <41244237+SioGabx@users.noreply.github.com>
2025-06-19 18:38:24 +0200

    CURVETOPOLYGON now can convert multiple polyligne at once

--- SioForgeCAD/Functions/CURVETOPOLYGON.cs
+++ SioForgeCAD/Functions/CURVETOPOLYGON.cs
@@ -3,40 +3,55 @@ using Autodesk.AutoCAD.EditorInput;
 using SioForgeCAD.Commun;
 using SioForgeCAD.Commun.Drawing;
 using SioForgeCAD.Commun.Extensions;
+using System;
 
 namespace SioForgeCAD.Functions
 {
     public static class CURVETOPOLYGON
     {
+        private static int LastConvertNumberOfSegmentPerArc = 3;
         public static void Convert()
         {
             Editor ed = Generic.GetEditor();
             var db = Generic.GetDatabase();
+
             using (Transaction tr = db.TransactionManager.StartTransaction())
             {
-                ObjectId OriginalPoly = ObjectId.Null;
-                using (var poly = ed.GetPolyline(out OriginalPoly, "Selectionnez une polyligne", Clone: false))
+                var PromptCurves = ed.GetCurves("Selectionnez des courbes à convertir", false);
+                if (PromptCurves.Status == PromptStatus.OK)
                 {
-                    if (poly == null) {
-                        tr.Commit();
-                        return; }
-                    int NumberOfVerticesBefore = poly.NumberOfVertices;
-
-                    if (!poly.IsWriteEnabled) { poly.UpgradeOpen(); }
-
+                    //Get number of segments per arc
                     PromptDoubleOptions promptDoubleOptions = new PromptDoubleOptions("Indiquez le nombre minimum de segments par arcs")
                     {
-                        DefaultValue = 3
+                        DefaultValue = LastConvertNumberOfSegmentPerArc
                     };
+
                     var value = ed.GetDouble(promptDoubleOptions);
-                    if (value.Status != PromptStatus.OK) { return; }
-                    var Polygon = poly.ToPolygon((uint)value.Value);
-                    var OriginalPolyEnt = OriginalPoly.GetEntity();
-                    OriginalPolyEnt.CopyPropertiesTo(Polygon);
-                    Polygon.AddToDrawing();
-                    OriginalPolyEnt.EraseObject();
-                    tr.Commit();
+                    if (value.Status != PromptStatus.OK)
+                    {
+                        tr.Commit();
+                        return;
+                    }
+                    LastConvertNumberOfSegmentPerArc = (int)Math.Floor(value.Value);
+                    //Convert all selected
+                    foreach (var item in PromptCurves.Value.GetObjectIds())
+                    {
+                        var Ent = item.GetDBObject(OpenMode.ForWrite);
+                        if (Ent is Curve curvEnt)
+                        {
+                            var EntAsPolyline = curvEnt.ToPolyline();
+                            using (EntAsPolyline)
+                            {
+                                var Polygon = EntAsPolyline.ToPolygon((uint)LastConvertNumberOfSegmentPerArc);
+                                curvEnt.CopyPropertiesTo(Polygon);
+                                Polygon.AddToDrawing();
+                                curvEnt.EraseObject();
+                            }
+                        }
+                    }
                 }
+
+                tr.Commit();
             }
         }
     }
