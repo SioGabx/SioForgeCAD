b421bd921c5de77ebfaec730c88fd5896027762e
SioGabx <41244237+SioGabx@users.noreply.github.com>
2025-07-24 11:53:51 +0200

    BLKSETTOBYBBLOCK can now do entity outside of blocks

--- SioForgeCAD/Functions/BLKSETTOBYBBLOCK.cs
+++ SioForgeCAD/Functions/BLKSETTOBYBBLOCK.cs
@@ -18,50 +18,71 @@ namespace SioForgeCAD.Functions
                 return;
             }
 
+            bool? ChangeEntitySelectedNotInsideBlock = null;
             using (Transaction tr = db.TransactionManager.StartTransaction())
             {
                 foreach (ObjectId blockRefId in ObjectIds)
                 {
-                    if (!(blockRefId.GetDBObject(OpenMode.ForWrite) is BlockReference blockRef))
+                    var SelEnt = blockRefId.GetDBObject(OpenMode.ForWrite) as Entity;
+                    if (SelEnt is BlockReference blockRef)
                     {
-                        return;
-                    }
-
-                    BlockTableRecord blockDef = blockRef.BlockTableRecord.GetObject(OpenMode.ForWrite) as BlockTableRecord;
-                    foreach (ObjectId EntityInBlockDef in blockDef)
-                    {
-                        if (EntityInBlockDef.GetDBObject(OpenMode.ForWrite) is Entity ent)
+                        BlockTableRecord blockDef = blockRef.BlockTableRecord.GetObject(OpenMode.ForWrite) as BlockTableRecord;
+                        foreach (ObjectId EntityInBlockDef in blockDef)
                         {
-                            if (ent is Hatch hatchEnt)
+                            if (EntityInBlockDef.GetDBObject(OpenMode.ForWrite) is Entity ent)
                             {
-                                switch (IgnoreHatch)
-                                {
-                                    case HatchSupport.Ignore:
-                                        continue;
-                                    case HatchSupport.Include:
-                                        SetEntityToByBloc(hatchEnt);
-                                        hatchEnt.BackgroundColor = Color.FromColorIndex(ColorMethod.ByBlock, 0);
-                                        break;
-                                    case HatchSupport.SetToWhite:
-                                        SetEntityToByBloc(hatchEnt);
-                                        hatchEnt.Color = Color.FromRgb(255, 255, 255);
-                                        hatchEnt.SetHatchPattern(HatchPatternType.PreDefined, "SOLID");
-                                        break;
-                                }
-                            }
-                            else
-                            {
-                                SetEntityToByBloc(ent);
+                                SetEntityToByBloc(ent, IgnoreHatch);
                             }
                         }
+                        foreach (ObjectId BlkRefInDrawing in blockDef.GetBlockReferenceIds(true, false))
+                        {
+                            (BlkRefInDrawing.GetDBObject(OpenMode.ForWrite) as BlockReference).RecordGraphicsModified(true);
+                        }
+
                     }
-                    foreach (ObjectId BlkRefInDrawing in blockDef.GetBlockReferenceIds(true, false))
+                    else
                     {
-                        (BlkRefInDrawing.GetDBObject(OpenMode.ForWrite) as BlockReference).RecordGraphicsModified(true);
+                        if (ChangeEntitySelectedNotInsideBlock is null)
+                        {
+                            var askResult = ed.GetOptions("Des entités en dehors d'un bloc ont été sélectionnées. Voulez-vous effectuer l'opération sur ces entités également ?", "Oui", "Non");
+                            ChangeEntitySelectedNotInsideBlock = askResult.Status == PromptStatus.OK && askResult.StringResult == "Oui" ? true : false;
+                        }
+                        if (ChangeEntitySelectedNotInsideBlock == true)
+                        {
+                            SetEntityToByBloc(SelEnt, IgnoreHatch);
+                        }
+
                     }
                 }
                 tr.Commit();
             }
+        }
+
+
+        private static void SetEntityToByBloc(Entity entity, HatchSupport hatchSupport)
+        {
+            if (entity is Hatch hatchEnt)
+            {
+                switch (hatchSupport)
+                {
+                    case HatchSupport.Ignore:
+                        return;
+                    case HatchSupport.Include:
+                        SetEntityToByBloc(hatchEnt);
+                        hatchEnt.BackgroundColor = Color.FromColorIndex(ColorMethod.ByBlock, 0);
+                        return;
+                    case HatchSupport.SetToWhite:
+                        SetEntityToByBloc(hatchEnt);
+                        hatchEnt.Color = Color.FromRgb(255, 255, 255);
+                        hatchEnt.SetHatchPattern(HatchPatternType.PreDefined, "SOLID");
+                        return;
+                }
+            }
+            else
+            {
+                SetEntityToByBloc(entity);
+            }
+
         }
 
         private static void SetEntityToByBloc(Entity entity)
