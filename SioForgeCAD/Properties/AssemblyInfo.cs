653f4e3ab23fe466eb5d5d8536784907627f155c
SioGabx <41244237+SioGabx@users.noreply.github.com>
2026-04-07 13:20:45 +0200

    update github action

--- SioForgeCAD/Properties/AssemblyInfo.cs
+++ SioForgeCAD/Properties/AssemblyInfo.cs
@@ -22,20 +22,9 @@ using System.Runtime.InteropServices;
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("2037e9c5-32f1-43d5-8433-45124dd5bee7")]
 
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire 
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
-// en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("1.0.*")]
-
 //Hot Reload and Edit & Continue are not compatible with using * in AssemblyVersionAttribute value.
 //Presence of * in the version means that the compiler generates a new version every build based on the current time.
 //The build then produces non-reproducible, non-deterministic outputs
-[assembly: AssemblyVersion("2026.4.7.7")]
-[assembly: AssemblyFileVersion("2026.4.7.7")]
+[assembly: AssemblyVersion("2026.4.7.9")]
+[assembly: AssemblyFileVersion("2026.4.7.9")]
 [assembly: NeutralResourcesLanguage("")]
